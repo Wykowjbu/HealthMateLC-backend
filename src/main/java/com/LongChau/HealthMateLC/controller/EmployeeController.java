cf078ef3194fc9eb5fdb37f2021015a904c99284
Wykowjbu <huynpde180519@fpt.edu.vn>
2025-06-26 16:05:31 +0700

    them moi chuc nang edit customer

--- src/main/java/com/LongChau/HealthMateLC/controller/EmployeeController.java
+++ src/main/java/com/LongChau/HealthMateLC/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.LongChau.HealthMateLC.controller;
 
 import com.LongChau.HealthMateLC.model.Customer;
 import com.LongChau.HealthMateLC.repository.CustomerRepository;
+import com.LongChau.HealthMateLC.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,20 +14,25 @@ import java.util.List;
 public class EmployeeController {
 
     @Autowired
-    private CustomerRepository customerRepository;
+    private CustomerService customerService;
 
 
     @GetMapping("/danh-sach-khach-hang")
     public ResponseEntity<List<Customer>> getAllCustomers(){
-        List<Customer> customers=customerRepository.findAll();
+        List<Customer> customers=customerService.getAll();
         return ResponseEntity.ok(customers);
     }
 
     @PostMapping("/tao-moi-khach-hang")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
-        Customer addNewCustomer=customerRepository.save(customer);
-
-
+        Customer addNewCustomer=customerService.addNewCustomer(customer);
         return ResponseEntity.ok(addNewCustomer);
     }
+
+    @PatchMapping("edit-customer-info")
+    public  ResponseEntity<Customer> editCustomer(@RequestBody Customer customer){
+        Customer editCustomer=customerService.updateCustomer(customer);
+        return ResponseEntity.ok(editCustomer);
+    }
+
 }
