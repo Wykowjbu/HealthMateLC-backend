cbd9c091fab740ac9d878d9fa26738f7fbb92efd
Wykowjbu <huynpde180519@fpt.edu.vn>
2025-07-01 13:09:05 +0700

    them moi chuhc nang chinh sua thong tin khahcs hang

--- src/main/java/com/LongChau/HealthMateLC/controller/EmployeeController.java
+++ src/main/java/com/LongChau/HealthMateLC/controller/EmployeeController.java
@@ -1,7 +1,6 @@
 package com.LongChau.HealthMateLC.controller;
 
 import com.LongChau.HealthMateLC.model.Customer;
-import com.LongChau.HealthMateLC.repository.CustomerRepository;
 import com.LongChau.HealthMateLC.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,14 +24,40 @@ public class EmployeeController {
 
     @PostMapping("/tao-moi-khach-hang")
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+        if (isPhoneOrEmailExists(customer)) {
+            return ResponseEntity.badRequest().body(customer);
+        }
         Customer addNewCustomer=customerService.addNewCustomer(customer);
         return ResponseEntity.ok(addNewCustomer);
     }
 
-    @PatchMapping("edit-customer-info")
-    public  ResponseEntity<Customer> editCustomer(@RequestBody Customer customer){
-        Customer editCustomer=customerService.updateCustomer(customer);
-        return ResponseEntity.ok(editCustomer);
+
+    @PutMapping("/cap-nhat-khach-hang/{id}")
+    public ResponseEntity<Customer> updateCustomerById(@PathVariable Integer id, @RequestBody Customer customer) {
+        // Set the customer ID from path variable
+        customer.setCustomerId(id);
+        if (isPhoneOrEmailExists(customer)) {
+            return ResponseEntity.badRequest().body(customer);
+        }
+        Customer updatedCustomer = customerService.updateCustomer(customer);
+        return ResponseEntity.ok(updatedCustomer);
     }
 
+    public boolean isPhoneOrEmailExists(Customer customer) {
+        List<Customer> customers = customerService.getAll();
+        for (Customer existingCustomer : customers) {
+            if (!existingCustomer.getCustomerId().equals(customer.getCustomerId())) {
+                if (existingCustomer.getPhone() != null && existingCustomer.getPhone().equals(customer.getPhone())) {
+                    return true;
+                }
+                if (existingCustomer.getEmail() != null && existingCustomer.getEmail().equals(customer.getEmail())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
+
