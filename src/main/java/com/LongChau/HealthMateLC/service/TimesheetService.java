88f8fd7d8dda2780dce83ab6c171e1206a221772
HoangTrinhyeuoi <hoangtrinh240705@gmail.com>
2025-08-01 00:18:41 +0700

    ResetPassword_BackEnd_Update_ListCustomer(Deleted)_Create LocalProperties

---
+++ src/main/java/com/LongChau/HealthMateLC/service/TimesheetService.java
@@ -0,0 +1,30 @@
+package com.LongChau.HealthMateLC.service;
+
+import com.LongChau.HealthMateLC.dto.CheckInGPSRequest;
+import com.LongChau.HealthMateLC.model.Timesheet;
+import com.LongChau.HealthMateLC.repository.TimesheetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Service
+public class TimesheetService {
+
+    @Autowired
+    private TimesheetRepository timesheetRepository;
+
+    public void checkInWithGPS(CheckInGPSRequest request, Principal principal) {
+        // Logic check-in và lưu GPS
+        Timesheet timesheet = new Timesheet();
+        // Set các thông tin cần thiết
+        timesheet.setCheckinLat(request.getLat());
+        timesheet.setCheckinLng(request.getLng());
+        timesheet.setCheckin(LocalTime.now());
+        timesheet.setDate(LocalDate.now());
+
+        timesheetRepository.save(timesheet);
+    }
+}
